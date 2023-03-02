4a6df16ff07bb2803f3f27dec8e8606504beb467
jacky-ui <jacky.du.004@gmail.com>
2023-03-01 19:41:35 -0500

    grab button and div container of lists from index.html and console.log. works and able to grab tag

--- script/script.js
+++ script/script.js
@@ -1,1 +1,4 @@
-console.log("hello world");
+const showMenu = document.querySelector("#showMenu")
+console.log(showMenu);
+let container = document.querySelector(".container");
+console.log(container);
