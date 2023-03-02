1623a509683a2f578b14ffb54e70a99c1640fb9e
jacky-ui <jacky.du.004@gmail.com>
2023-03-01 19:50:22 -0500

    add testing code within eventlistener and was able to compare classlist value of button

--- script/script.js
+++ script/script.js
@@ -1,6 +1,12 @@
 const showMenu = document.querySelector("#showMenu")
 let container = document.querySelector(".container");
+console.log(container.classList.value);
 
 showMenu.addEventListener("click", () => {
-    console.log("hello world");
+    
+    if (container.classList.value === "container hide--content") {
+        console.log("hello world");
+    } else {
+        console.log("nope")
+    }
 });
