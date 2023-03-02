3acb5c26a632b5b101c07a931e77efbc94787eea
jacky-ui <jacky.du.004@gmail.com>
2023-03-01 19:54:18 -0500

    add comment on javascript file

--- script/script.js
+++ script/script.js
@@ -1,9 +1,8 @@
 const showMenu = document.querySelector("#showMenu")
 let container = document.querySelector(".container");
-console.log(container.classList.value);
 
+// Event Listener to show or hide menu when clicked on
 showMenu.addEventListener("click", () => {
-    
     if (container.classList.value === "container hide--content") {
         container.classList.remove("hide--content");
     } else {
