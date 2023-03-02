c728ef2a0c7bc5b92914b9fd1ea24fbeb3aed763
jacky-ui <jacky.du.004@gmail.com>
2023-03-01 19:43:20 -0500

    add click eventListener to button and have it console.log hello world for testing. works

--- script/script.js
+++ script/script.js
@@ -1,4 +1,6 @@
 const showMenu = document.querySelector("#showMenu")
-console.log(showMenu);
 let container = document.querySelector(".container");
-console.log(container);
+
+showMenu.addEventListener("click", () => {
+    console.log("hello world");
+});
